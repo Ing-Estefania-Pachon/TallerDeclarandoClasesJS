eee3ee0ea9fdd09dd0eadbe8a9fc325eb275b481
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-15 15:18:58 -0500

    Agregue la solución de los ejercicios 1 y 2

--- 1_sistema_Vehiculos.js
+++ 1_sistema_Vehiculos.js
@@ -3,6 +3,62 @@
 
 // 1. Una clase Vehiculo con propiedades: marca, modelo, año y un 
 //   método obtenerDetalles() que devuelva un string con toda la información.
+class Vehiculo{
+    constructor(marca, modelo, año){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+    }
+
+    obtenerDetalles(){
+        console.log("La información del vehículo es: ");
+        console.log("La marca es: ", this.marca);
+        console.log("El modelo es: ", this.modelo);
+        console.log("El año es: ", this.año);
+    }
+}
+
+const vehiculo1 = new Vehiculo("Toyota", "RTX", 2024)
+vehiculo1.obtenerDetalles()
+
 //  2. Una clase Coche que herede de Vehiculo y añada la propiedad numeroPuertas.
+
+// ! HERENCIA
+
+class Coche extends Vehiculo{
+    constructor(marca, modelo, año, numeroPuertas){
+        super(marca, modelo, año)
+        this.numeroPuertas = numeroPuertas;
+    }
+    obtenerDetalles(){
+        console.log("La información del coche es: ");
+        console.log("La marca es: ", this.marca);
+        console.log("El modelo es: ", this.modelo);
+        console.log("El año es: ", this.año);
+        console.log("La cantidad de puertas es: ", this.numeroPuertas);
+    }
+}
+
+const coche1 = new Coche("Toyota", "RTX", 2024, 2)
+coche1.obtenerDetalles();
+
 //  3. Una clase Moto que herede de Vehiculo y añada la propiedad cilindrada.
-//  Crea instancias de ambas clases hijas y muestra sus detalles.
+
+// ! HERENCIA
+
+class Moto extends Vehiculo{
+    constructor(marca, modelo, año, cilindrada){
+        super(marca, modelo, año)
+        this.cilindrada = cilindrada;
+    }
+    obtenerDetalles(){
+        console.log("La información de la moto es: ");
+        console.log("La marca es: ", this.marca);
+        console.log("El modelo es: ", this.modelo);
+        console.log("El año es: ", this.año);
+        console.log("La cilindrada de la moto es: ", this.cilindrada);
+    }
+}
+
+const moto1 = new Moto("Toyota", "RTX", 2024, 125)
+moto1.obtenerDetalles();
--- 2_sistema_Empleados.js
+++ 2_sistema_Empleados.js
@@ -3,7 +3,53 @@
 
 //  1.Clase Empleado con propiedades: nombre, edad, salarioBase y 
 // método calcularSalario() que devuelve el salario base.
+class Empleado{
+    constructor(nombre, edad, salarioBase){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.salarioBase = salarioBase;
+    }
+
+    calcularSalario(){
+        console.log("Empleado ", this.nombre);
+        console.log("El salario base es: ", this.salarioBase);
+    }
+}
+
+const empleado1 = new Empleado("Juan", "23", 500);
+empleado1.calcularSalario();
+
 //  2.Clase Gerente que herede de Empleado y añada bonificacion. 
 // Sobrescribe calcularSalario() para que sume la bonificación.
+
+class Gerente extends Empleado{
+    constructor(nombre, edad, salarioBase, bonificacion){
+        super (nombre, edad, salarioBase);
+        this.bonificacion = bonificacion;
+    }
+
+    calcularSalario(){
+        let salarioTotal = this.salarioBase + this.bonificacion
+        console.log("Gerente ", this.nombre);
+        console.log("El salario total es: ", salarioTotal);
+    }
+}
+
+const gerente1 =  new Gerente("Luis", 23, 500, 20);
+gerente1.calcularSalario()
+
 //  3.Clase Desarrollador que herede de Empleado y añada lenguaje. El salario es igual al base.
-//  Crea instancias de cada tipo y muestra sus salarios.
+class Desarrollador extends Empleado{
+    constructor(nombre, edad, salarioBase, lenguaje){
+        super (nombre, edad, salarioBase);
+        this.lenguaje = lenguaje;
+    }
+    calcularSalario(){
+        console.log("Desarrollador ", this.nombre);
+        console.log("El salario base es: ", this.salarioBase);
+
+    }
+}
+
+const desarrollador1 = new Desarrollador("Jim", 23, 500, "Python");
+desarrollador1.calcularSalario()
