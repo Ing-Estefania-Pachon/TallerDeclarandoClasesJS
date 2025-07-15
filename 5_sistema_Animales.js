ec5eeb0ec6de4db3632b2616c6a9b28403f3c7bd
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-15 15:57:14 -0500

    Agregue la respuesta del ejercicio 5

--- 5_sistema_Animales.js
+++ 5_sistema_Animales.js
@@ -2,6 +2,44 @@
 //  Modela un sistema de animales con:
 
 //  1.Clase Animal con nombre, edad y método emitirSonido().
+class Animal{
+    constructor(nombre, edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    emitirSonido(){
+        console.log("El animal emitié un sonido ");
+    }
+}
+
 //  2.Clase Perro que herede de Animal y sobrescriba emitirSonido().
+class Perro extends Animal{
+    constructor(nombre, edad){
+        super (nombre, edad)
+    }
+
+    emitirSonido(){
+        console.log("El perro hace Guau");
+    }
+}
+
 //  3.Clase Gato que herede de Animal y sobrescriba emitirSonido().
-//  Crea instancias y haz que emitan sonidos
+class Gato extends Animal{
+    constructor(nombre, edad){
+        super (nombre, edad)
+    }
+
+    emitirSonido(){
+        console.log("El gato hace Miau");
+    }
+}
+
+const animal1 = new Animal("Pepillo", 8);
+animal1.area();
+
+const perro1 = new Perro("Kira", 5)
+perro1.area();
+
+const gato1 = new Gato("Lucha", 4)
+gato1.area();
