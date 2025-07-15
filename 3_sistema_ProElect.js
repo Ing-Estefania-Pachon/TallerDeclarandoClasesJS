356becbdceeed450f5a8f0a0c2a13a380a98015b
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-15 15:50:32 -0500

    Agregue la solución del ejercicio 3 y 4

--- 3_sistema_ProElect.js
+++ 3_sistema_ProElect.js
@@ -2,6 +2,59 @@
 //  Modela un sistema de productos electrónicos con:
 
 //  1.Clase Producto con nombre, precio, marca y método descripcion().
+class Producto{
+    constructor(nombre, precio, marca){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.marca = marca;
+    }
+    descripcion(){
+        console.log("Información del producto: ");
+        console.log("El nombre del producto es: ", this.nombre);
+        console.log("El precio del producto es: ", this.precio);
+        console.log("La marca del producto es: ", this.marca);        
+    }
+}
+
 //  2.Clase Telefono que herede de Producto y añada almacenamiento, ram.
+class Telefono extends Producto{
+    constructor(nombre, precio, marca, almacenamiento, ram){
+        super(nombre, precio, marca);
+        this.almacenamiento = almacenamiento;
+        this.ram = ram;
+    }
+    descripcion(){
+        console.log("Información del teléfono: ");
+        console.log("El nombre del producto es: ", this.nombre);
+        console.log("El precio del producto es: ", this.precio);
+        console.log("La marca del producto es: ", this.marca);        
+        console.log("La capacidad de almacenamiento es: ", this.almacenamiento);        
+        console.log("La RAM es de: ", this.ram);        
+    }
+}
+
 //  3.Clase Laptop que herede de Producto y añada procesador, pulgadas.
-//  Crea instancias y muestra sus descripciones.
+class Laptop extends Producto{
+    constructor(nombre, precio, marca, procesador, pulgadas){
+        super (nombre, precio, marca);
+        this.procesador = procesador;
+        this.pulgadas = pulgadas;
+    }
+    descripcion(){
+        console.log("Información de la Laptop: ");
+        console.log("El nombre del producto es: ", this.nombre);
+        console.log("El precio del producto es: ", this.precio);
+        console.log("La marca del producto es: ", this.marca);        
+        console.log("El procesador es: ", this.procesador);        
+        console.log("Las pulgadas de la pantalla son: ", this.pulgadas);  
+    }
+}
+
+const producto1 = new Producto("Audifonos", 300, "Samsung")
+producto1.descripcion()
+
+const telefono1 = new Telefono("A33", 300, "Samsung", "126 gb", 8)
+telefono1.descripcion()
+
+const laptop1 = new Laptop("Portatil", 2400, "HP", "Intel", 13.3)
+laptop1.descripcion()
--- 4_sistema_FigGeome.js
+++ 4_sistema_FigGeome.js
@@ -2,6 +2,46 @@
 //  Crea un sistema de clases para figuras geométricas:
 
 //  1.Clase Figura con propiedad color y método area() que devuelve 0.
+class Figura{
+    constructor(color){
+        this.color = color;
+    }
+    area(){
+        let area = 0
+        console.log("El area de la figura es: ", area);
+    }
+}
+
 //  2.Clase Rectangulo que herede de Figura y añada base, altura. Sobrescribe area().
+class Rectangulo extends Figura{
+    constructor(color, base, altura){
+        super (color);
+        this.base = base;
+        this.altura = altura;
+    }
+    area(){
+        let area = this.base * this.altura
+        console.log("El área del rectángulo es: ", area);
+    }
+}
+
 //  3.Clase Circulo que herede de Figura y añada radio. Sobrescribe area().
-//  Crea instancias y muestra sus áreas.
+class Circulo extends Figura{
+    constructor(color, radio){
+        super (color);
+        this.radio = radio;
+    }
+    area(){
+        let area = (Math.PI * this.radio)**2
+        console.log("El área del rectángulo es: ", area);
+    }
+}
+
+const figura1 = new Figura("Azul");
+figura1.area();
+
+const rectangulo1 = new Rectangulo("Amarillo", 2, 4)
+rectangulo1.area();
+
+const circulo1 = new Circulo("Morado", 5)
+circulo1.area();
